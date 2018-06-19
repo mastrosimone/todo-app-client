83020f4129318410b9b6f427df3ae12711886dea
mastrosimone <ale.mastrosimone@gmail.com>
2018-06-20 01:20:00 +0200

    19 giugno

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,23 +8,15 @@
 
         window.app.Session.get();
         window.app.Store.getTodos(function(response){
+        console.log(response)
         response = JSON.parse(response);
-            console.log(response)
+        console.log(response)
 
         })
-        // for(x = 0;x < data.content.lenght;x++)
-
-
-        var center = document.querySelector('#center')
-        var div = document.createElement("div")
-        div.textContent = ''
-        // div.textContent = json.data.docs
-        center.appendChild(div)
-        
-        // for(i = 0;i < json.data.length;i++)
-
 
         
+        
+
         window.app.Session.get()
 
             var form = document.querySelector('#insert_form')
@@ -34,20 +26,55 @@
 
                 event.preventDefault()
                 var content = document.querySelector('#nota').value
+                
                 var json = {}
                 json.data = {}
                 json.data.uuid = window.app.Session.get()
                 json.data.content = content
                 json.data.done = false
 
+                form.reset()
+
                 json = JSON.stringify(json);
                 
                 window.app.Store.createTodo(json, function(response) {
-                    response = JSON.parse(response);
+                console.log('submit prima del parse:', response)
+                response = JSON.parse(response);
+                console.log('submit dopo il parse:',response)
+
+                var center = document.getElementById('center');
+                var div = document.createElement("div");
+                div.classList.add("div_class");
+
+                var input = document.createElement("input")
+                input.classList.add("checkbox")
+                input.setAttribute("type", "checkbox")
+
+                var space = document.createElement("space")
+                input.classList.add("space")
+
+                var h1 = document.createElement("h1");
+                h1.textContent = response.data.content
+
+                div.appendChild(input)
+                div.appendChild(h1);
+                div.appendChild(space)
+                center.appendChild(div);
+               
+                
 
                 })
 
-            // xhr.send(json);
+                /* window.app.Store.updateTodo(json, function(response) {
+                    console.log('submit prima del parse:', response)
+                    response = JSON.parse(response);
+                    console.log('submit dopo il parse:',response)
+                   
+                    
+    
+                }) */
+
+             
                     
         });
             
--- assets/js/store.js
+++ assets/js/store.js
@@ -3,18 +3,21 @@
 
 //64a30e80-c75c-836e-0eba-e8ae9629e45e
 
+//2fd7b454-301c-f8b1-84b0-4eed488e0e1d
+
 (function(window, session) {
      var sessionUuid = session.get();
 
     function updateTodo(id, todoModel, callbackFunction) {
         // ajax call to post method
+        console.log('ciao')
     }
 
     function createTodo(todoModel, callbackFunction) {
         // ajax call to post method
         var xhr = new XMLHttpRequest(),
         method = "POST",
-        url = "http://192.168.20.173:7000/todos";
+        url = "http://todoapp.mosfarm.eu/todos";
     
         xhr.open(method, url, true);
         xhr.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
@@ -31,7 +34,7 @@
 
         var xhr = new XMLHttpRequest(),
         method = "GET",
-        url = "http://192.168.20.173:7000/todos"
+        url = "http://todoapp.mosfarm.eu/todos"
         url = url + '?uuid=' + sessionUuid;
 
         xhr.open(method, url, true);
