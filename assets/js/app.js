21ff3ec170830f10d63c5028018d03aecb5a30c5
MDA <mda@I-Costa.local>
2018-06-19 14:08:21 +0200

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,53 @@
     // Please read me! https://stackoverflow.com/a/8835458
     window.addEventListener('load', function() {
 
+
+        window.app.Session.get();
+        window.app.Store.getTodos(function(response){
+        response = JSON.parse(response);
+            console.log(response)
+
+        })
+        // for(x = 0;x < data.content.lenght;x++)
+
+
+        var center = document.querySelector('#center')
+        var div = document.createElement("div")
+        div.textContent = ''
+        // div.textContent = json.data.docs
+        center.appendChild(div)
+        
+        // for(i = 0;i < json.data.length;i++)
+
+
+        
+        window.app.Session.get()
+
+            var form = document.querySelector('#insert_form')
+            form.addEventListener('submit', function() {
+                
+                window.app.Session.get()
+
+                event.preventDefault()
+                var content = document.querySelector('#nota').value
+                var json = {}
+                json.data = {}
+                json.data.uuid = window.app.Session.get()
+                json.data.content = content
+                json.data.done = false
+
+                json = JSON.stringify(json);
+                
+                window.app.Store.createTodo(json, function(response) {
+                    response = JSON.parse(response);
+
+                })
+
+            // xhr.send(json);
+                    
+        });
+            
     });
+    
+    
 })(window, document);
--- assets/js/store.js
+++ assets/js/store.js
@@ -1,7 +1,10 @@
 "use strict";
 
+
+//64a30e80-c75c-836e-0eba-e8ae9629e45e
+
 (function(window, session) {
-    // var sessionUuid = session.get();
+     var sessionUuid = session.get();
 
     function updateTodo(id, todoModel, callbackFunction) {
         // ajax call to post method
@@ -9,10 +12,36 @@
 
     function createTodo(todoModel, callbackFunction) {
         // ajax call to post method
+        var xhr = new XMLHttpRequest(),
+        method = "POST",
+        url = "http://192.168.20.173:7000/todos";
+    
+        xhr.open(method, url, true);
+        xhr.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
+
+        xhr.addEventListener('load', function (x) {
+            callbackFunction(x.target.response);
+        });
+
+        xhr.send(todoModel);
+
     }
 
     function getTodos(callbackFunction) {
-        // ajax get call
+
+        var xhr = new XMLHttpRequest(),
+        method = "GET",
+        url = "http://192.168.20.173:7000/todos"
+        url = url + '?uuid=' + sessionUuid;
+
+        xhr.open(method, url, true);
+
+        xhr.addEventListener('load', function (x) {
+            callbackFunction(x.target.response);
+        });
+
+        xhr.send();
+
     }
 
     // Export to window
